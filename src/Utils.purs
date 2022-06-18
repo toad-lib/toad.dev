7ef698c518d1a76cd05d3592c9d010563e718b09
Orion Kindel <cakekindel@gmail.com>
2022-06-18 14:19:50 -0700

    cool guy

--- src/Utils.purs
+++ src/Utils.purs
@@ -1,19 +1,17 @@
 module Utils where
 
 import Prelude
-import Data.Array (fromFoldable)
 import Data.Foldable (class Foldable)
-import Data.Maybe (Maybe(..))
 import Halogen.HTML.Properties as Props
 import Halogen.HTML.Core (ClassName(..))
 
 classes :: ∀ r i. Array String -> Props.IProp ( class :: String | r ) i
 classes = map ClassName >>> Props.classes
 
-appendFoldable :: ∀ f a. Foldable f => Array a -> f a -> Array a
-appendFoldable = flip $ fromFoldable >>> append
+appendFoldable :: ∀ f s a. Foldable f => Semigroup (s a) => (f ~> s) -> s a -> f a -> s a
+appendFoldable fromFoldable = flip $ fromFoldable >>> append
 
-test :: ∀ a. Boolean -> a -> Maybe a
-test true a = Just a
+test :: ∀ f a. Applicative f => Monoid (f a) => Boolean -> a -> f a
+test true = pure
 
-test false a = Nothing
+test false = const mempty
