b84c27f38ff3e3b61f5a7b014768f5c880b62a83
Orion Kindel <cakekindel@gmail.com>
2022-06-25 11:57:58 -0700

    fix: improve rendering at extreme zoom levels

--- src/Kwap.App.Logo.purs
+++ src/Kwap.App.Logo.purs
@@ -1,7 +1,7 @@
 module Kwap.App.Logo (render) where
 
 import Kwap.App.Css
-import Prelude hiding (top, bottom)
+import Prelude hiding (bottom, top)
 
 import CSS.Common as Css.Common
 import Kwap.App.Html as HH
@@ -9,7 +9,7 @@ import Kwap.App.Html as HH
 foreign import kwapMaskUrl :: String
 
 maskImageRatio :: Number
-maskImageRatio = 500.0 / 1200.0
+maskImageRatio = 5.0 / 12.0
 
 maskWidthRem :: Number
 maskWidthRem = 8.0
@@ -17,26 +17,40 @@ maskWidthRem = 8.0
 maskHeightRem :: Number
 maskHeightRem = maskWidthRem * maskImageRatio
 
+paddingRem :: Number
+paddingRem = 1.0
+
+overlap :: Number
+overlap = 0.3
+
+maskOutlineInnerWidthRem :: Number
+maskOutlineInnerWidthRem = maskWidthRem - overlap
+
+maskOutlineInnerHeightRem :: Number
+maskOutlineInnerHeightRem = maskHeightRem - overlap
+
 render :: ∀ w i. HH.HTML w i
 render = HH.div
-  [style do
-           position relative
-           display flex
-           justifyContent Css.Common.center
-           alignItems Css.Common.center
-           height $ rem maskHeightRem
-           width $ rem maskWidthRem
-           sym padding $ rem 1.0
+  [ style do
+      position relative
+      display flex
+      justifyContent Css.Common.center
+      alignItems Css.Common.center
+      height $ rem maskHeightRem
+      width $ rem maskWidthRem
+      sym padding $ rem paddingRem
   ]
-  [ HH.div [style do
-             position absolute
-             height $ rem (maskHeightRem - 0.025)
-             width $ rem (maskWidthRem - 0.025)
-             border solid (rem 1.0) (cssColor $ Yellow Lightest)
-             sym borderRadius $ rem 0.5
-             top $ rem 0.025
-             left $ rem 0.025
-           ] []
+  [ HH.div
+      [ style do
+          position absolute
+          height $ rem maskOutlineInnerHeightRem
+          width $ rem maskOutlineInnerWidthRem
+          top $ rem (overlap / 2.0)
+          left $ rem (overlap / 2.0)
+          border solid (rem paddingRem) (cssColor $ Yellow Lightest)
+          sym borderRadius $ rem 0.5
+      ]
+      []
   , HH.div
       [ style do
           width $ rem maskWidthRem
