cc8bd6b1621861c7e03f874f4a12caaeab237cb6
Orion Kindel <cakekindel@gmail.com>
2022-07-15 19:43:52 -0700

    fix: new manifest url

--- src/Kwap/Concept.Fetch.purs
+++ src/Kwap/Concept.Fetch.purs
@@ -2,33 +2,17 @@ module Kwap.Concept.Fetch ( manifest ) where
 
 import Prelude
 
-import Data.Argonaut.Core (jsonNull) as Text.Json
-import Data.Argonaut.Parser (jsonParser) as Text.Json
-import Data.Bifunctor (lmap)
-import Data.Codec.Argonaut
-  ( JsonCodec
-  , array
-  , decode
-  , printJsonDecodeError
-  , string
-  ) as Text.Json
-import Data.Codec.Argonaut ((>~>))
-import Data.Codec.Argonaut.Compat (maybe) as Text.Json
-import Data.Codec.Argonaut.Migration (addDefaultField) as Text.Json
-import Data.Codec.Argonaut.Record (object) as Text.Json
 import Data.Either (Either)
-import Data.Maybe (Maybe, fromMaybe)
 import Effect.Aff (Aff)
 import Effect.Aff.Fetch as HTTP
-import Kwap.Concept (Manifest(..), decodeManifest)
+import Kwap.Concept (Manifest, decodeManifest)
 
-baseUrl :: String
-baseUrl =
-  "https://raw.githubusercontent.com/clov-coffee/kwap-docs/main/concepts"
+manifestUrl :: String
+manifestUrl =
+  "https://raw.githubusercontent.com/clov-coffee/kwap-docs/main/concepts.json"
 
 manifest :: HTTP.FetchImpl -> Aff (Either String Manifest)
 manifest impl =
-  HTTP.fetch impl (HTTP.URL $ baseUrl <> "/index.json") HTTP.Get mempty
+  HTTP.fetch impl (HTTP.URL manifestUrl) HTTP.Get mempty
     >>= HTTP.text
     <#> decodeManifest
-
